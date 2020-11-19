73ff51872d224a13fc0602e4d9a960bab93ebd9c
Yupiel <stefan_mitrovic@outlook.com>
2020-11-19 16:18:53 +0100

    Added Basic Setup
    index html/css/ts #7 #8 #9
    ts and tsconfig #3
    dockerfile #1
    webpack & config #4
    webpack loaders
    jest #6
    express #5
    
    node project #2
    
    Webpakc config build successfull as of this commit

---
+++ webpack.dev.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                use: [
+                    'html-loader'
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+      extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output:{
+        path: path.resolve(__dirname, 'dist', 'dev'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist', 'dev'),
+        compress: true,
+        port: 9000
+    }
+};
