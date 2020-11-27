9be117c93bf36be75fa3a54b79f8323031a18c5d
Yupiel <stefan_mitrovic@outlook.com>
2020-11-27 11:35:47 +0100

    Changes to Path and removal of unused require in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
     mode: 'production',
@@ -40,7 +39,7 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        path: path.resolve(__dirname, 'dist', 'dev'),
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({ template: './src/index.html' }),
